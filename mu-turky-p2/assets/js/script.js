96834bb44e84fa29b702446bbf5f6dfc887edfe6
NajeedUrRehman <74587097+najeedurrehman@users.noreply.github.com>
2024-11-30 15:07:04 +0500

    Table Content Fixed

--- mu-turky-p2/assets/js/script.js
+++ mu-turky-p2/assets/js/script.js
@@ -1,79 +1,48 @@
-// const header = document.querySelector(".header__nav");
-// document.addEventListener("scroll", (event) => {
-//   const scroll = this.scrollY;
-//   if (scroll > 70) {
-//     header.classList.add("glossy-header");
-//   } else {
-//     header.classList.remove("glossy-header");
-//   }
-// });
+const toolbar = document.querySelector(".toolbar");
+const searchbar = document.querySelector(".search-bar");
 
-// dataBody.addEventListener("scroll", (event) => {
-//   const scroll = dataBody.scrollTop;
-//   if (scroll > 20) {
-//     tableContentBlock.classList.add("hide-content-block");
-//     statsTable.classList.add("p-0");
-//     stattableContainer.classList.add("mt-0");
-//     tableHeader.classList.add("w-100");
-//     tableHeader.classList.add("header-plr");
-//     dataBody.classList.add("pl-30");
-//   } else {
-//     tableContentBlock.classList.remove("hide-content-block");
-//     statsTable.classList.remove("p-0");
-//     stattableContainer.classList.remove("mt-0");
-//     tableHeader.classList.remove("w-100");
-//     tableHeader.classList.remove("header-plr");
-//     dataBody.classList.  ("pl-30");
-//   }
-// });
+const table = document.querySelector(".table");
+const tableHeader = document.querySelector(".table__header");
+const tableBody = document.querySelector(".table__body");
 
-// const tableBody = document.querySelector(".stats-table-body");
-// const statsTable = document.querySelector(".stats-table");
-// const tableHeader = document.querySelector(".stats-table-header");
-// const statTableContainer = document.querySelector(".stat-table-container");
-// const tableContentBlock = document.querySelector(".table-content-block");
-// const dataBody = document.querySelector("#data-body");
+const contentContainer = document.querySelector(".content-container");
+document.addEventListener("scroll", (event) => {
+  const scroll = this.scrollY;
 
-// dataBody.addEventListener("scroll", (event) => {
-//   const scroll = dataBody.scrollTop;
+  if (scroll > 10) {
+    toolbar.classList.remove("animate__fadeInRight");
+    toolbar.classList.add("animate__fadeOutRight");
 
-//   if (scroll > 20) {
-//     statsTable.classList.add("p-0");
-//     tableContentBlock.classList.add("d-none");
-//     statTableContainer.classList.add("mt-0");
-//     tableHeader.classList.add("header-plr");
-//     tableHeader.classList.add("header-w-full");
-//     dataBody.classList.add("pl-30");
-//   } else {
-//     statsTable.classList.remove("p-0");
-//     statTableContainer.classList.remove("mt-0");
-//     tableHeader.classList.remove("header-plr");
-//     tableHeader.classList.remove("header-w-full");
-//     dataBody.classList.remove("pl-30");
-//     tableContentBlock.classList.remove("d-none");
-//   }
-// });
+    searchbar.classList.remove("animate__fadeInLeft");
+    searchbar.classList.add("animate__fadeOutLeft");
 
-// dataBody.addEventListener("scroll", (event) => {
-//   const scroll = dataBody.scrollTop;
+    /* TABLE CHANGES */
+    table.classList.add('w-full');
+    tableHeader.classList.add('plr-header');
+    tableBody.classList.add('table__body-customize');
+    /* */
+    setTimeout(() => {
+      toolbar.classList.remove("d-flex");
+      searchbar.classList.remove("d-flex");
+    }, 100);
+  } else {
+    toolbar.classList.remove("animate__fadeOutRight");
+    toolbar.classList.add("animate__fadeInRight");
 
-//   if (scroll > 20) {
-//     tableHeader.classList.add("default-width");
-//     tableHeader.classList.remove("header-plr");
-//     statsTable.classList.remove("active-state-table");
-//     dataBody.classList.add("pl-0");
-//     tableContentBlock.classList.add("hide-content-block");
-//   } else {
-//     tableContentBlock.classList.remove("hide-content-block");
-//     tableHeader.classList.remove("default-width");
-//     tableHeader.classList.add("header-plr");
-//     statsTable.classList.add("active-state-table");
-//     dataBody.classList.remove("pl-0");
-//   }
-// });
+    searchbar.classList.remove("animate__fadeOutLeft");
+    searchbar.classList.add("animate__fadeInLeft");
 
-// const showAll = document.querySelector(".show-all");
+    
+    /* TABLE CHANGES */
+    table.classList.remove('w-full');
+    tableHeader.classList.remove('plr-header');
+    tableBody.classList.remove('table__body-customize');
+    /* */
 
-// function TriggerAnimation() {
-//   alert('Welcome');
-// }
+
+    setTimeout(() => {
+      toolbar.classList.add("d-flex");
+      searchbar.classList.add("d-flex");
+    }, 100);
+  }
+});
