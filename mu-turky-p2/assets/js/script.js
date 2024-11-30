591e21c6f63a4b0ce24468e3b98298c512ca8837
NajeedUrRehman <74587097+najeedurrehman@users.noreply.github.com>
2024-11-30 18:09:28 +0500

    Header Fixed Changes

--- mu-turky-p2/assets/js/script.js
+++ mu-turky-p2/assets/js/script.js
@@ -2,14 +2,17 @@ const toolbar = document.querySelector(".toolbar");
 const searchbar = document.querySelector(".search-bar");
 
 const table = document.querySelector(".table");
+
 const tableHeader = document.querySelector(".table__header");
+const tableHeaderFixed = document.querySelector(".table__header-fixed");
+
 const tableBody = document.querySelector(".table__body");
 
 const contentContainer = document.querySelector(".content-container");
 document.addEventListener("scroll", (event) => {
   const scroll = this.scrollY;
 
-  if (scroll > 10) {
+  if (scroll > 70) {
     toolbar.classList.remove("animate__fadeInRight");
     toolbar.classList.add("animate__fadeOutRight");
 
@@ -17,9 +20,13 @@ document.addEventListener("scroll", (event) => {
     searchbar.classList.add("animate__fadeOutLeft");
 
     /* TABLE CHANGES */
-    table.classList.add('w-full');
-    tableHeader.classList.add('plr-header');
-    tableBody.classList.add('table__body-customize');
+    table.classList.add("w-full");
+    tableHeader.classList.add("d-none");
+
+    
+    tableHeaderFixed.classList.add("d-flex");
+
+    tableBody.classList.add("table__body-customize");
     /* */
     setTimeout(() => {
       toolbar.classList.remove("d-flex");
@@ -32,13 +39,12 @@ document.addEventListener("scroll", (event) => {
     searchbar.classList.remove("animate__fadeOutLeft");
     searchbar.classList.add("animate__fadeInLeft");
 
-    
     /* TABLE CHANGES */
-    table.classList.remove('w-full');
-    tableHeader.classList.remove('plr-header');
-    tableBody.classList.remove('table__body-customize');
+    table.classList.remove("w-full");
+    tableHeader.classList.remove("d-none");
+    tableHeaderFixed.classList.remove("d-flex");
+    tableBody.classList.remove("table__body-customize");
     /* */
-
 
     setTimeout(() => {
       toolbar.classList.add("d-flex");
